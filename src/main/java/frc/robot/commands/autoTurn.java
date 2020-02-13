beae5a5e5e6cd0735ba41cdc5cb9e74faee851f2
NathanM-FRC <nmatte78@students.byron.k12.mn.us>
2020-02-13 17:22:47 -0600

    I've made some progress on the Auto Speed has been lowered until I can find a better fix

--- src/main/java/frc/robot/commands/autoTurn.java
+++ src/main/java/frc/robot/commands/autoTurn.java
@@ -58,6 +58,7 @@ public class autoTurn extends Command {
     @Override
     protected void end() {
         Robot.driveTrain.stop();
+        
     }
 
     // Called when another command which requires one or more of the same
