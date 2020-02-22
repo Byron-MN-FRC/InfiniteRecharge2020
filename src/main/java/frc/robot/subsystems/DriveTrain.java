b1b47fcf1b662d4544810c0e26aeb423391bf10c
rubiks29 <zbower13@students.byron.k12.mn.us>
2020-02-21 20:54:29 -0600

    added slow mode

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -378,7 +378,152 @@ addChild("coolingSolenoidMotors",coolingSolenoidMotors);
     //rightMaster.configPeakOutputReverse(-max, Constants.kTimeoutMs);
 
     }
+    public void motorConfigFalconSlow() {
+        // Factory default all motors initially
+        rightMaster.configFactoryDefault();
+        rightFollower.configFactoryDefault();
+        leftMaster.configFactoryDefault();
+        leftFollower.configFactoryDefault();
 
+        // Set Neutral Mode
+        leftMaster.setNeutralMode(NeutralMode.Brake);
+        leftFollower.setNeutralMode(NeutralMode.Brake);
+        rightMaster.setNeutralMode(NeutralMode.Brake);
+        rightFollower.setNeutralMode(NeutralMode.Brake);
+
+        // Configure output and sensor direction
+        leftMaster.setInverted(_leftInvert);
+        leftFollower.setInverted(_leftInvert);
+        rightMaster.setInverted(_rightInvert);
+        rightFollower.setInverted(_rightInvert);
+
+        // configure the max current for motor. Thought is that
+        // other motors will follow.
+        double ampLimit = SmartDashboard.getNumber(Constants.kAmpLimitName_DT, Constants.kAmpLimit_DT);
+        double ampPeakLimit = SmartDashboard.getNumber(Constants.kAmpPeakLimitName_DT, Constants.kAmpPeak_DT);
+
+        SupplyCurrentLimitConfiguration currentLimitingFalcons = new SupplyCurrentLimitConfiguration(
+                Constants.kEnableCurrentLimiting_DT, ampLimit, ampPeakLimit, Constants.kthreshholdTime);
+
+        rightMaster.configSupplyCurrentLimit(currentLimitingFalcons);
+
+        // Reset Pigeon Configs
+        pigeon.configFactoryDefault();
+
+        _leftConfig.primaryPID.selectedFeedbackSensor = FeedbackDevice.IntegratedSensor; // Local Feedback Source
+
+        // * Configure the Remote (Left) Talon's selected sensor as a remote sensor for
+        // * the right Talon
+
+        _rightConfig.remoteFilter0.remoteSensorDeviceID = leftMaster.getDeviceID(); // Device ID of Remote Source
+        _rightConfig.remoteFilter0.remoteSensorSource = RemoteSensorSource.TalonSRX_SelectedSensor; // Remote Source
+                                                                                                    // Type
+
+        // * Now that the Left sensor can be used by the master Talon, set up the Left
+        // * (Aux) and Right (Master) distance into a single Robot distance as the
+        // * Master's Selected Sensor 0.
+
+        setRobotDistanceConfigs(_rightInvert, _rightConfig);
+        // FPID for Distance
+        _rightConfig.slot0.kF = Constants.kGains_Distanc.kF;
+        _rightConfig.slot0.kP = Constants.kGains_Distanc.kP;
+        _rightConfig.slot0.kI = Constants.kGains_Distanc.kI;
+        _rightConfig.slot0.kD = Constants.kGains_Distanc.kD;
+        _rightConfig.slot0.integralZone = Constants.kGains_Distanc.kIzone;
+        _rightConfig.slot0.closedLoopPeakOutput = Constants.kGains_Distanc.kPeakOutput;
+
+        // * Heading Configs
+        _rightConfig.remoteFilter1.remoteSensorDeviceID = pigeon.getDeviceID(); // Pigeon Device ID
+        _rightConfig.remoteFilter1.remoteSensorSource = RemoteSensorSource.Pigeon_Yaw; // This is for a Pigeon over CAN
+        _rightConfig.auxiliaryPID.selectedFeedbackSensor = FeedbackDevice.RemoteSensor1; // Set as the Aux Sensor
+        _rightConfig.auxiliaryPID.selectedFeedbackCoefficient = 3600.0 / Constants.kPigeonUnitsPerRotation; // Convert
+                                                                                                            // Yaw to
+                                                                                                            // tenths of
+                                                                                                            // a degree
+
+        // * false means talon's local output is PID0 + PID1, and other side Talon is
+        // PID0
+        // * - PID1 This is typical when the master is the right Talon FX and using
+        // Pigeon
+        // *
+        // * true means talon's local output is PID0 - PID1, and other side Talon is
+        // PID0
+        // * + PID1 This is typical when the master is the left Talon FX and using
+        // Pigeon
+
+        _rightConfig.auxPIDPolarity = false;
+        // FPID for Heading
+        _rightConfig.slot1.kF = Constants.kGains_Turning.kF;
+        _rightConfig.slot1.kP = Constants.kGains_Turning.kP;
+        _rightConfig.slot1.kI = Constants.kGains_Turning.kI;
+        _rightConfig.slot1.kD = Constants.kGains_Turning.kD;
+        _rightConfig.slot1.integralZone = Constants.kGains_Turning.kIzone;
+        _rightConfig.slot1.closedLoopPeakOutput = Constants.kGains_Turning.kPeakOutput;
+
+        // Config the neutral deadband.
+        _leftConfig.neutralDeadband = Constants.kNeutralDeadband;
+        _rightConfig.neutralDeadband = Constants.kNeutralDeadband;
+
+        // *
+        // * 1ms per loop. PID loop can be slowed down if need be. For example, - if
+        // * sensor updates are too slow - sensor deltas are very small per update, so
+        // * derivative error never gets large enough to be useful. - sensor movement is
+        // * very slow causing the derivative error to be near zero.
+
+        int closedLoopTimeMs = 1;
+        rightMaster.configClosedLoopPeriod(0, closedLoopTimeMs, Constants.kTimeoutMs);
+        rightMaster.configClosedLoopPeriod(1, closedLoopTimeMs, Constants.kTimeoutMs);
+
+        // Motion Magic Configs
+        _rightConfig.motionAcceleration = 3250; // (distance units per 100 ms) per second
+        _rightConfig.motionCruiseVelocity = 7500; // distance units per 100 ms
+
+        // APPLY the config settings
+        leftMaster.configAllSettings(_leftConfig);
+        leftFollower.configAllSettings(_leftConfig);
+        rightMaster.configAllSettings(_rightConfig);
+        rightFollower.configAllSettings(_rightConfig);
+
+        // Set status frame periods to ensure we don't have stale data
+
+        // * These aren't configs (they're not persistant) so we can set these after the
+        // * configs.
+
+        rightMaster.setStatusFramePeriod(StatusFrame.Status_12_Feedback1, 20, Constants.kTimeoutMs);
+        rightMaster.setStatusFramePeriod(StatusFrame.Status_13_Base_PIDF0, 20, Constants.kTimeoutMs);
+        rightMaster.setStatusFramePeriod(StatusFrame.Status_14_Turn_PIDF1, 20, Constants.kTimeoutMs);
+        rightMaster.setStatusFramePeriod(StatusFrame.Status_10_Targets, 10, Constants.kTimeoutMs);
+        leftMaster.setStatusFramePeriod(StatusFrame.Status_2_Feedback0, 5, Constants.kTimeoutMs);
+        pigeon.setStatusFramePeriod(PigeonIMU_StatusFrame.CondStatus_9_SixDeg_YPR, 5, Constants.kTimeoutMs);
+       
+
+        rightFollower.follow(rightMaster);
+        leftFollower.follow(leftMaster);
+        // WPI drivetrain classes assume by default left & right are opposite
+        // - call this to apply + to both sides when moving forward
+        tankDrive.setRightSideInverted(false);
+
+        // set on call from autonomous
+        rightMaster.selectProfileSlot(Constants.kSlot_Distanc, Constants.PID_PRIMARY);
+        rightMaster.selectProfileSlot(Constants.kSlot_Turning, Constants.PID_TURN);
+        // rightFollower.follow(rightMaster);
+        // leftFollower.follow(leftMaster);
+
+        // rightMaster.configStatorCurrentLimit(currLimitCfg)
+        // (Constants.kPeakCurrentAmps, Constants.kTimeoutMs);
+        // rightMaster.configPeakCurrentDuration(Constants.kPeakTimeMs,
+        // Constants.kTimeoutMs);
+        // rightMaster.configContinuousCurrentLimit(Constants.kContinCurrentAmps,
+        // Constants.kTimeoutMs);
+        // rightMaster.enableCurrentLimit(_currentLimEn); // Honor initial setting
+           //Set Max output of motors
+    double max = 0.25;
+    leftMaster.configPeakOutputForward(max, Constants.kTimeoutMs);
+    // leftMaster.configPeakOutputReverse(-max, Constants.kTimeoutMs);
+    rightMaster.configPeakOutputForward(max, Constants.kTimeoutMs);
+    //rightMaster.configPeakOutputReverse(-max, Constants.kTimeoutMs);
+
+    }
     /** Zero all sensors, both Talons and Pigeon */
     public void zeroSensors() {
         leftMaster.getSensorCollection().setIntegratedSensorPosition(0, Constants.kTimeoutMs);
